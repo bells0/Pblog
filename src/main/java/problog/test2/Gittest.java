b401f358dafdb5e4667c2b3a43bb54aab6b5359c
Linjiaqiang <3108136635@qq.com>
2019-08-17 11:23:20 +0800

    删除test目录以及里面的文件，重新添加目录test2文件,在此目录下添加一个Gittest.java文件，用于测试git

--- src/main/java/problog/test2/Gittest.java
+++ src/main/java/problog/test2/Gittest.java
@@ -1,7 +1,7 @@
-package problog.test;
+package problog.test2;
 
 public class Gittest {
     public static void main(String[] args) {
-        System.out.println("测试git");
+        System.out.println("时代");
     }
 }
